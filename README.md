74471213e3b03f7c3046536879caffcb92e5bc40
Ankit kumar <iamankitm05@gmail.com>
2026-02-10 22:24:02 +0530

    initial setup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Music Player
+# music_player
 
-Simple yet powerful Music Player, exclusively designed for playing system audio files. Enjoy smooth playback, customizable playlists, and effortless file management for your favorite tunes.
+A new Flutter project.
--- lib/constants/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-abstract final class AppColors {
-  static const Color deepPurple = Color(0xFF5E17EB);
-  static const Color grey = Colors.grey;
-  static const Color white = Colors.white;
-  static const Color black = Colors.black;
-  static final Color purple100 = Colors.purple.shade100;
-}
